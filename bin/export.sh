c8b15afbc5b0d16ed4515f6bb6bee99755b50af2
Alexander Matsievsky <matsievsky@scrapinghub.com>
2021-01-08 16:09:20 +0300

    feat: update to 2021

---
+++ bin/export.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -ex
+
+npx resume export --resume=assets/resume.json --theme=elegant assets/resume.html
+npx resume export --resume=assets/resume.json --theme=flat /tmp/README.html
+npx resume export --resume=assets/resume.json --theme=paper assets/resume.pdf
+
+cat - <(
+  node -p 'new require("turndown")().turndown(fs.readFileSync(0).toString())' </tmp/README.html | tail -n+2
+) <<EOF >README.md
+[[HTML version]](https://raw.githubusercontent.com/alexander-matsievsky/resume/master/assets/resume.html)
+[[PDF version]](https://raw.githubusercontent.com/alexander-matsievsky/resume/master/assets/resume.pdf)
+EOF
