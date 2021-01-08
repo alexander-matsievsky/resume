fc490c775a69c419925a9da4f821876ce3998ebf
Alexander Matsievsky <matsievsky@scrapinghub.com>
2021-01-08 17:18:30 +0300

    build: serve via *raw.githack.com*

--- bin/export.sh
+++ bin/export.sh
@@ -9,6 +9,6 @@ npx resume export --resume=assets/resume.json --theme=paper assets/resume.pdf
 cat - <(
   node -p 'new require("turndown")().turndown(fs.readFileSync(0).toString())' </tmp/README.html | tail -n+2
 ) <<EOF >README.md
-[[HTML version]](https://raw.githubusercontent.com/alexander-matsievsky/resume/master/assets/resume.html)
-[[PDF version]](https://raw.githubusercontent.com/alexander-matsievsky/resume/master/assets/resume.pdf)
+[[HTML version]](https://raw.githack.com/alexander-matsievsky/resume/master/assets/resume.html)
+[[PDF version]](https://raw.githack.com/alexander-matsievsky/resume/master/assets/resume.pdf)
 EOF
